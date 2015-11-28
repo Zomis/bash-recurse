ba9bddbe7d6068f5f9b4a7ffda6d8f5f6aeac5a9
Simon Forsberg <zomis2k@hotmail.com>
2015-11-28 14:35:40 +0100

    Add example script and directory structure for future testing

---
+++ example/filecheck.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ -e "ok.txt" ]; then
+    cat ok.txt
+    exit 0
+fi
+
+if [ -e "fail.txt" ]; then
+    echo "Fail found"
+    exit 1
+fi
+
+if [ -e "fail2.txt" ]; then
+    echo "Fail 2 found"
+    exit 2
+fi
+
+exit 3
+
